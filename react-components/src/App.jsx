dacc82ad4eba1cf4b1feb94f9e5dd8849b1db61a
lecio19 <alex.macri01@gmail.com>
2024-11-27 18:07:19 +0100

    add footer

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "../components/header"
+import Card from '../components/card'
+import Footer from "../components/footer"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-       {/* TITOLO */}
       <Header />
       <Card />
       <Footer />
